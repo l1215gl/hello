223a4b593c75f98f69e19ab7a3388393705e640b
l1215gl <59405952+l1215gl@users.noreply.github.com>
2020-01-06 16:24:24 +0800

    Update Hello.java

--- Hello/src/hello/Hello.java
+++ Hello/src/hello/Hello.java
@@ -3,9 +3,7 @@ package hello;
 public class Hello {
 	public static void main(String[] args) {
 		String a = "123";
-		System.out.println("======");
 		System.out.println("======"+a);
-		System.out.println("==");
 	}
 
 }
